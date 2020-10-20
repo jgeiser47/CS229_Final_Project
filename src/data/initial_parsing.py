a7da7e8667e4cc917e8057bdebb1ec1e2badfbbc
Joshua Geiser <joshuageiser@DN0a1ea3f4.SUNet>
2020-10-19 21:06:56 -0700

    Updated dataset to include load data and datetime column (also realized LA load data has some missing entries

--- src/data/initial_parsing.py
+++ src/data/initial_parsing.py
@@ -78,11 +78,14 @@ def parse_data(input_filepath, output_filepath):
     n_hours = 24 # hours in a day
     
     # Headers and thus number of columns
-    header = ['date', 'hour', 'dwpc', 'relh', 'sped', 'tmpc']
-    n_cols = 6
+    header = ['date', 'hour', 'date_time', 'dwpc', 'relh', 'sped', 'tmpc', 'load']
+    n_cols = len(header)
     
-    # Read in input datafile using pandas
+    # Read in input weather datafile using pandas
     df_in = pd.read_csv(input_filepath)
+    
+    # Also read in input load datafile using pandas
+    df_load = pd.read_csv(input_filepath.replace('_weather.csv', '_load.csv'))
     
     # Array of length 24 denoting strings for each hour (i.e. '0:00')
     hours_arr = np.array(list(df_in.keys()[2:]))
@@ -103,11 +106,24 @@ def parse_data(input_filepath, output_filepath):
         # Array of length 24 with current date as each entry (i.e. '9/28/20')
         dates_arr = np.full(n_hours, date_curr)
         
+        # Array of length 24 with datetime for each entry
+        date_time_arr = [date_curr+' '+hour for hour in hours_arr]
+        
         # Array of length 4x24 containing weather data for each hour on date
         chunk_arr = np.array(chunk[hours_arr])
         
+        # Array of length 24x1 with load data for specified date
+        # Try-except block is only needed for caiso_la data as there is some
+        # missing data in that load dataset
+        try:
+            i_load = df_load.index[df_load['date'] == date_curr][0]
+            load_data = np.array(df_load[hours_arr].loc[i_load])
+            load_data = np.nan_to_num(load_data)
+        except IndexError:
+            load_data = np.zeros((n_hours))
+        
         # Array of length 24x6 with current chunk data
-        full_chunk = np.vstack((dates_arr,hours_arr,chunk_arr)).T
+        full_chunk = np.vstack((dates_arr,hours_arr,date_time_arr, chunk_arr, load_data)).T
         
         # Add current chunk to output dataframe
         df_out = np.vstack((df_out, full_chunk))
