ae32d3263759797f89c7541375cbf62010af8064
Fletcher Passow <fletcherpassow@icloud.com>
2020-11-07 13:09:39 -0500

    Implement create_window_tensor for input variables

--- src/models/rnn_model_keras.py
+++ src/models/rnn_model_keras.py
@@ -12,7 +12,22 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 from base_model_sklearn import get_data
 
+def create_window_tensor(X, window):
+    """Preprocess data using keras for the RNN
+    
+    Args:
+        X: matrix of inputs for the RNN (num_steps, num_features)
+        window: the width of the look-back window used for the RNN
 
+    Returns: 3-dimensional tensor (np.ndarray) of dimensions
+    (num_steps, window, num_features) ready for input to an LSTM
+    """
+    window_list = []
+    for i in range(window-1, X.shape[0]):
+        cur_window = X[(i-window):i,:]
+        window_list.append(cur_window)
+    window_list = window_list[1:len(window_list)-1]
+    return window_list
 
 def main():
     # Path to data directory contaning CSVs 
@@ -30,7 +45,8 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
     
     # Train model and predict
-    
+    X_train_list = create_window_tensor(X_train, window = 6)
+    #TODO: Select columns for use in the NN
     
     # Scatter plot of predictions vs true values
     plt.figure()
