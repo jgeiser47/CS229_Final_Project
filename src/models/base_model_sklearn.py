758b0b01b121499adb8b92a6fa641c12db620c56
Joshua Geiser <joshuageiser@DN51sl9p.SUNet>
2020-11-08 12:14:26 -0800

    Updated base model 1

--- src/models/base_model_sklearn.py
+++ src/models/base_model_sklearn.py
@@ -9,10 +9,40 @@ Created on Wed Oct 21 19:24:18 2020
 import os
 import pandas as pd
 import numpy as np
+import datetime
 from matplotlib import pyplot as plt
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+
+def get_datetime(dt_str):
+    
+    # year = int(dt_str.split('-')[0])
+    # month = int(dt_str.split('-')[1])
+    # day = int(dt_str.split('-')[2].split(' ')[0])
+    
+    # hour = int(dt_str.split(' ')[1].split(':')[0])
+    # minute = 0
+    # second = 0
+    
+    dt_obj = datetime.datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
+    
+    return dt_obj
+
+def to_dt(dt_dataframe):
+    
+    N = len(dt_dataframe)
+    
+    return_arr = [''] * N
+    
+    j = 0
+    #for i in range(N):
+    for i in dt_dataframe.index:
+        return_arr[j] = get_datetime(dt_dataframe[i])
+        j+=1
+    
+    return return_arr
+
 
 def get_data(data_dir, region, city):
     
@@ -44,7 +74,19 @@ def get_data(data_dir, region, city):
     y = np.array(y)
     #y = y[:,0]
     
-    return X, y
+    # Split off December 2019 into a separate validation set for timeseries
+    # data visualization
+    slice_index = 25536 - start_index
+    X_test = X[slice_index:]
+    y_test = y[slice_index:]
+    X = X[:slice_index]
+    y = y[:slice_index]
+    
+    # Also need the dates range for plotting timeseries
+    dates_range_test = df['date_hour'][slice_index+start_index:end_index]
+    
+    return X, y, X_test, y_test, dates_range_test
+
 
 def main():
     
@@ -57,30 +99,48 @@ def main():
     city = 'houston'
     
     # Get design matrix and labels
-    X,y = get_data(data_dir, region, city)
+    X,y,X_test,y_test,dates_range_test = get_data(data_dir, region, city)
     
     # Split into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.33, random_state=42)
     
     # Train model and predict 
     mdl = linear_model.LinearRegression()
     mdl.fit(X_train, y_train)
-    y_pred = mdl.predict(X_test)
+    y_pred = mdl.predict(X_val)
     
     # Scatter plot of predictions vs true values
     plt.figure()
-    plt.scatter(y_test, y_pred)
-    plt.xlabel('True Load')
-    plt.ylabel('Predicted Load')
+    plt.scatter(y_val, y_pred)
+    plt.xlabel('True Load (MW)')
+    plt.ylabel('Predicted Load (MW)')
+    plt.title('Linear Regression Predicted vs True Load')
     plt.show()
     
     # Error statistics
-    MSE = mean_squared_error(y_test, y_pred)
-    R2_score = r2_score(y_test, y_pred)
+    MSE = mean_squared_error(y_val, y_pred)
+    R2_score = r2_score(y_val, y_pred)
     
     print(f'Mean Squared Error: {MSE:.2f}')
     print(f'R2 Score: {R2_score:.3f}')
- 
+    
+    ##########################################################################
+    
+    y_test_pred = mdl.predict(X_test)
+    dates_arr = to_dt(dates_range_test)
+    
+    plt.figure()
+    
+    start_ind = 0
+    end_ind = 350
+    plt.plot_date(dates_arr[start_ind:end_ind], y_test[start_ind:end_ind], 'b:', label='True')
+    plt.plot_date(dates_arr[start_ind:end_ind], y_test_pred[start_ind:end_ind], 'g:', label='Predicted')
+    plt.xticks(rotation=45)
+    plt.xlabel('Date')
+    plt.ylabel('Load (MW)')
+    plt.title('Linear Regression 2-Week Predicted vs True Load')
+    plt.legend()
+    plt.show()
     
     return
 
